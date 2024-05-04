121479a395579aea8b43b94af6075893b54911b9
Le Doan Tan Tri <ledoantantri@gmail.com>
2024-05-04 23:54:01 +0700

    add datepicker

--- src/components/inputs/datepicker.js
+++ src/components/inputs/datepicker.js
@@ -1,63 +1,27 @@
-// "use client"
+'use client'
+import React from "react";
+import {DateRangePicker} from "@nextui-org/react";
+import {getLocalTimeZone, today} from "@internationalized/date";
+import {I18nProvider} from "@react-aria/i18n";
 
-// import * as React from "react"
-// import { addDays, format } from "date-fns"
-// import { Calendar as CalendarIcon } from "lucide-react"
-// import { DateRange } from "react-day-picker"
 
-// import { cn } from "../others/lib/utils"
-// import { Button } from "../others/ui/button"
-// import {
-//   Popover,
-//   PopoverContent,
-//   PopoverTrigger,
-// } from "../others/ui/popover"
-
-// export default function DatePickerWithRange({className}) {
-
-//   const [date, setDate] = React.useState<DateRange | undefined>({
-//     from: new Date(2022, 0, 20),
-//     to: addDays(new Date(2022, 0, 20), 20),
-//   })
-
-//   return (
-//     <div className={cn("grid gap-2", className)}>
-//       <Popover>
-//         <PopoverTrigger asChild>
-//           <Button
-//             id="date"
-//             variant={"outline"}
-//             className={cn(
-//               "w-[300px] justify-start text-left font-normal",
-//               !date && "text-muted-foreground"
-//             )}
-//           >
-//             <CalendarIcon className="mr-2 h-4 w-4" />
-//             {date?.from ? (
-//               date.to ? (
-//                 <>
-//                   {format(date.from, "LLL dd, y")} -{" "}
-//                   {format(date.to, "LLL dd, y")}
-//                 </>
-//               ) : (
-//                 format(date.from, "LLL dd, y")
-//               )
-//             ) : (
-//               <span>Pick a date</span>
-//             )}
-//           </Button>
-//         </PopoverTrigger>
-//         <PopoverContent className="w-auto p-0" align="start">
-//           <Calendar
-//             initialFocus
-//             mode="range"
-//             defaultMonth={date?.from}
-//             selected={date}
-//             onSelect={setDate}
-//             numberOfMonths={2}
-//           />
-//         </PopoverContent>
-//       </Popover>
-//     </div>
-//   )
-// }
+export default function DatePicker() {
+    return (
+        <div className="flex w-full">
+            <I18nProvider locale="vi-VI">
+                <DateRangePicker 
+                    label="Chọn ngày"
+                    variant="bordered"
+                    visibleMonths={2}
+                    pageBehavior="single"
+                    minValue={today(getLocalTimeZone())}
+                    defaultValue={{
+                        start: today(getLocalTimeZone()),
+                        end: today(getLocalTimeZone()).add({days: 1}),
+                    }}
+                    
+                />
+            </I18nProvider>
+        </div>
+    )
+}
