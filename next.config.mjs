fcb4d550f58ea8fbca336429b6fbbbb552982e70
Le Doan Tan Tri <ledoantantri@gmail.com>
2024-05-20 03:19:08 +0700

    adjust img src to get remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'www.sofitel-legend-metropole-hanoi.com', 
-            'lh3.googleusercontent.com',
-            'cf.bstatic.com',
-            'd2e5ushqwiltxm.cloudfront.net', 
-            'www.ahstatic.com', 
-            'ik.imagekit.io',
-            'sofitel-saigon-plaza.com'
-        ],
+        remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**",
+            },
+          ],      
     },
 };
 
