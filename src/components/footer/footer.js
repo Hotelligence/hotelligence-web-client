6f706f87128103a170bb0f0fab564b5c73377978
Le Doan Tan Tri <ledoantantri@gmail.com>
2025-02-11 21:29:26 +0700

    change header and footer layout

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,20 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className="px-24 h-20 flex items-center justify-center bg-[var(--primary-blue-100)] text-[var(--primary-gold-100)] relative w-full text-center mt-20">
-            <p>
-            Bản quyền © 2024 Hotelligence™. Bảo lưu mọi quyền. <br />
-            Hotelligence và logo Hotelligence là thương hiệu hoặc thương hiệu đã đăng ký bảo hộ của Hotelligence, L.P.
-            </p>
-        </div>
+        <footer className="flex flex-col px-[100px] gap-4 bg-[var(--primary-blue-100)] text-[var(--primary-gold-100)] relative w-full mt-20">
+            <div className="flex flex-row justify-between mt-8">
+                <div className="flex flex-col justify-center gap-4 h-20 place-items-start">
+                    <Link href="/aboutUs">Về chúng tôi</Link>
+                    <Link href="/help">Hỗ trợ</Link>   
+                </div>
+            </div>
+            <div className="h-20 flex items-center justify-start opacity-65">
+                <p>
+                Bản quyền © 2024 Hotelligence™. Bảo lưu mọi quyền. <br />
+                Hotelligence và logo Hotelligence là thương hiệu hoặc thương hiệu đã đăng ký bảo hộ của Hotelligence, L.P.
+                </p>
+            </div>
+        </footer>
     );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,55 +1,64 @@
-import Logo from "../../images/Hotelligence Logo.png"
+import Logo from "../../images/Hotelligence Logo.png";
 import Image from "next/image";
 import Link from "next/link";
-// import { useAuth } from '@clerk/nextjs';  
+// import { useAuth } from '@clerk/nextjs';
 import { UserButton } from "@clerk/nextjs";
 import { CreateOrganization } from "@clerk/nextjs";
-import getCountByUserId from "../../api/hotel/getCountByUserId"
-import { auth } from '@clerk/nextjs/server'
+import getCountByUserId from "../../api/hotel/getCountByUserId";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Header() {
-    const { userId } = auth();  
+  const { userId } = auth();
 
-    const count = await getCountByUserId(userId);
-    
-    console.log(userId)
-    console.log(count)
-    
-    return (
-        <header className="px-[100px] h-20 grid grid-cols-[1fr_auto_1fr] items-center bg-[var(--primary-blue-100)] text-[var(--primary-gold-100)] sticky top-0 z-[1000] mb-10">
-            <div className="flex flex-row justify-start gap-[25px]">
-                <Link href="/aboutUs">Về chúng tôi</Link>
-                <Link href="/help">Hỗ trợ</Link>   
-            </div>
+  const count = await getCountByUserId(userId);
 
-            <div className="w-fit flex justify-center px-5">
-                <Link href="/">
-                    <Image src={Logo} height={60} alt="Hotelligence Logo" priority isZoomed={true}></Image>
-                </Link>
-            </div>
+  console.log(userId);
+  console.log(count);
 
-            <div className="flex flex-row justify-end gap-[25px]">                
-                {userId ? (
-                    <>
-                        {(count <= 0 || count === null) && (
-                            <Link href="/partnerRegister">Đăng ký trở thành Đối tác</Link>                    
-                        )}
+  return (
+    <header className="px-[100px] h-20 grid grid-cols-[1fr_auto_1fr] items-center bg-[var(--primary-blue-100)] text-[var(--primary-gold-100)] sticky top-0 z-[1000] mb-10">
+      <div className="flex flex-row justify-start gap-[25px]">
+        {userId && (
+          <>
+            <Link href={`/myFavorites/${userId}`}>Danh sách yêu thích</Link>
+            <Link href={`/myComparison/${userId}`}>So sánh phòng</Link>
+          </>
+        )}
+      </div>
 
-                        {(count > 0) && (
-                            <Link href={`/hotelManagement/${userId}`}>Quản lý khách sạn</Link>
-                        )}
+      <div className="w-fit flex justify-center px-5">
+        <Link href="/">
+          <Image
+            src={Logo}
+            height={60}
+            alt="Hotelligence Logo"
+            priority
+          ></Image>
+        </Link>
+      </div>
 
-                        <Link href="/bookingHistory">Lịch sử đặt phòng</Link>
-                        <UserButton afterSignOutUrl="/"/>
-                    </>
-                ) : (
-                    <>
-                        <Link href="/partnerRegister">Đăng ký trở thành Đối tác</Link>                    
-                        <Link href="/loginOrSignUp">Đăng ký</Link>
-                        <Link href="/loginOrSignUp">Đăng nhập</Link>
-                    </>
-                )}
-            </div>
-        </header>
-    );
+      <div className="flex flex-row justify-end gap-[25px]">
+        {userId ? (
+          <>
+            {(count <= 0 || count === null) && (
+              <Link href="/partnerRegister">Đăng ký trở thành Đối tác</Link>
+            )}
+
+            {count > 0 && (
+              <Link href={`/hotelManagement/${userId}`}>Quản lý khách sạn</Link>
+            )}
+
+            <Link href="/bookingHistory">Lịch sử đặt phòng</Link>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        ) : (
+          <>
+            <Link href="/partnerRegister">Đăng ký trở thành Đối tác</Link>
+            <Link href="/loginOrSignUp">Đăng ký</Link>
+            <Link href="/loginOrSignUp">Đăng nhập</Link>
+          </>
+        )}
+      </div>
+    </header>
+  );
 }
